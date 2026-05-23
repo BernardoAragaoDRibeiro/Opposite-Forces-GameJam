01445aeaaef18d99fa89d7e728a13d21bee1c3a1
Stuffy06138 <bernardoadribeiro03@gmail.com>
2026-05-23 16:12:33 -0300

    Update

---
+++ Assets/Scripts/EnemyHead.cs
@@ -0,0 +1,44 @@
+using StarterAssets;
+using UnityEngine;
+
+public class EnemyHead : MonoBehaviour
+{
+    [Tooltip("Minimal velocity to cause damage (m/s)")]
+    public float MinImpactVelocity = 3f;
+    [Tooltip("Velocity to max damage (m/s)")]
+    public float MaxImpactVelocity = 20f;
+    [Tooltip("Max damage caused")]
+    public float MaxDamage = 100f;
+    [Tooltip("Bounce strength hitting enemy")]
+    public float BounceStrength = 8f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        var controller = other.GetComponent<FirstPersonController>();
+        if (controller == null) return;
+
+        float fallSpeed = Mathf.Abs(controller.LastFallVelocity);
+        
+        // Small fall won't count
+        if (fallSpeed < MinImpactVelocity) return;
+        
+        // Maps fall velocity for damage
+        float damageRatio = Mathf.InverseLerp(MinImpactVelocity, MaxImpactVelocity, fallSpeed);
+        float damage = damageRatio * MaxDamage;
+        
+        GetComponentInParent<Enemy>().TakeDamage(damage);
+        controller.Bounce(BounceStrength);
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
